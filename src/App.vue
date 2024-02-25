6766692665e7dace64f498ab389e25a03ce28159
HellsFlame <ak6253160@gmail.com>
2024-02-25 05:53:44 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <VApp>
+    <Sidebar @selectColorOption="onSelectOption" />
+    <VMain>
+      <ImageGallery :isWithColor="isWithColor" />
+    </VMain>
+  </VApp>
+</template>
+
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+import ImageGallery from "./components/ImageGallery.vue";
+import { ref } from "vue";
+
+const isWithColor = ref(true);
+
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor;
+};
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <VNavigationDrawer expand-on-hover rail>
+    <VList>
+      <VListItem
+        prepend-avatar="https://media.licdn.com/dms/image/C4D03AQFZxkNEftgZDQ/profile-displayphoto-shrink_100_100/0/1647686816681?e=1714608000&v=beta&t=E3LcQXgy_wWvMiBg8NKtp9g3w-7WqujFVDOdhYc1TjQ"
+        title="Areeb Khan"
+        subtitle="ak6253160@gmailcom"
+      ></VListItem>
+    </VList>
+
+    <VDivider />
+
+    <VList density="compact" nav>
+      <VListItem
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectColorOption(true)"
+      />
+      <VListItem
+        prepend-icon="mdi-dialpad"
+        title="Grayspace Gallery"
+        value="grayspace"
+        @click="selectColorOption(false)"
+      />
+    </VList>
+  </VNavigationDrawer>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+const emits = defineEmits(["selectColorOption"]);
+
+const selectColorOption = (isColor) => {
+  emits("selectColorOption", isColor);
+};
+</script>
